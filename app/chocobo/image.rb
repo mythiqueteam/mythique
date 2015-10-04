a4352b575964c1b26653542e409cc8a4c43c535c
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-10-04 16:40:12 +0200

    feat : display background with chocobo methods

---
+++ app/chocobo/image.rb
@@ -0,0 +1,39 @@
+module Chocobo
+  class Image < Choco
+
+    def self.get_texture(image)
+      get_image("textures/#{image}")
+    end
+
+    def self.get_sprite(image)
+      get_image("sprites/#{image}")
+    end
+
+    def self.get_element(image)
+      get_image("elements/#{image}")
+    end
+
+    def self.element_size(image)
+      image_size("elements/#{image}")
+    end
+
+    def self.sprite_size(image)
+      image_size("sprites/#{image}")
+    end
+
+    def self.texture_size(image)
+      image_size("textures/#{image}")
+    end
+
+    private
+    def self.image_size(image)
+      image = get_image("#{image}")
+
+      { width: image.size.width, height: image.size.height }
+    end
+
+    def self.get_image(image)
+      MG::Sprite.new("images/#{image}")
+    end
+  end
+end
--- app/main_scene.rb
+++ app/main_scene.rb
@@ -1,5 +1,34 @@
 class MainScene < MG::Scene
   def initialize
     Chocobo::Audio.play_theme("main-theme.mp3")
+    @backgrounds = []
+    add_background
+  end
+
+  def add_background
+    x = 0
+    @backgrounds << []
+
+    img_size  = Chocobo::Image.texture_size("floor.png")
+
+    round_height  = (Chocobo::Screen.size[:height] / img_size[:height]).floor
+    round_width   = (Chocobo::Screen.size[:width] / img_size[:width]).floor
+
+    y = Chocobo::Screen.size[:height] - (img_size[:height] / 2)
+
+    round_height.times do
+
+      round_width.times do
+        floor = Chocobo::Image.get_texture("floor.png")
+        floor.position = [x, y]
+        add floor
+
+        x += img_size[:width]
+        @backgrounds.last << floor
+      end
+
+      x = 0
+      y -= img_size[:height]
+    end
   end
 end
