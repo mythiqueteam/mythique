a1111f4ba0757c14e5faff23b1046fd78fd8f1d4
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-10-05 22:25:28 +0200

    feat : display wall on main scene

--- app/main_scene.rb
+++ app/main_scene.rb
@@ -2,14 +2,17 @@ class MainScene < MG::Scene
   def initialize
     Chocobo::Audio.play_theme("main-theme.mp3")
     @backgrounds = []
-    add_background
+
+    add_floor
+    add_walls
+
   end
 
-  def add_background
+  def add_floor
     x = 0
     @backgrounds << []
 
-    img_size  = Chocobo::Image.texture_size("floor.png")
+    img_size  = Chocobo::Image.texture_size("floor-2.png")
 
     round_height  = (Chocobo::Screen.size[:height] / img_size[:height]).floor
     round_width   = (Chocobo::Screen.size[:width] / img_size[:width]).floor
@@ -19,7 +22,7 @@ class MainScene < MG::Scene
     round_height.times do
 
       round_width.times do
-        floor = Chocobo::Image.get_texture("floor.png")
+        floor = Chocobo::Image.get_texture("floor-2.png")
         floor.position = [x, y]
         add floor
 
@@ -29,6 +32,27 @@ class MainScene < MG::Scene
 
       x = 0
       y -= img_size[:height]
+    end
+  end
+
+  def add_walls
+    @backgrounds << []
+    img_size  = Chocobo::Image.texture_size("wall.png")
+
+    round_height  = (Chocobo::Screen.size[:height] / img_size[:height]).floor
+    round_width   = (Chocobo::Screen.size[:width] / img_size[:width]).floor
+    y = Chocobo::Screen.size[:height] - (img_size[:height] / 2)
+    x = 0 + (img_size[:width] / 2)
+
+    round_height.times do
+        wall = Chocobo::Image.get_texture("wall.png")
+        wall.position = [x, y]
+        wall.attach_physics_box
+        wall.dynamic = false
+        add wall
+
+        y -= img_size[:height]
+        @backgrounds.last << wall
     end
   end
 end
