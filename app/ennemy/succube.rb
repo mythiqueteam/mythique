307025bed4f0f89118979134a58c0d46d584bd31
ldocao <ldocao@gmail.com>
2015-10-15 19:58:45 +0200

    feat: create first monster succube

--- app/ennemy/succube.rb
+++ app/ennemy/succube.rb
@@ -1,8 +1,18 @@
+require_relative "../stats/status.rb"
+require_relative "../stats/stats.rb"
+require_relative "../stats/level.rb"
+require_relative "./monster.rb"
+require_relative "./ennemy.rb"
+
+
 module Ennemy
 
     class Succube < Monster
-
-
+        def initialize(status)
+            @name = "Succube"
+            @level = Stats::Level.new(1)
+            @status = status
+        end
     end
 
 end
