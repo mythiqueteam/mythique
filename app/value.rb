babb5c2ea7404f3910befe8b0d6e43f28177e86f
ldocao <ldocao@gmail.com>
2015-10-15 14:34:09 +0200

    feat: express value with units

--- app/value.rb
+++ app/value.rb
@@ -1,6 +1,24 @@
 # unit: for example $ for money. Should be immutable, once created ?
 #
 # Value that can take only a value in a given range, eg: Strength [0,MAX_STATS] or gold [0, MAX_GOLD]
+
+
+# val: value in float
+# unit: usually string
+# max: max value possible
+#
+# Prints out conveniently the value
+def express(val, unit, max=nil)
+    if max == nil
+        val.to_s + " " + unit.to_s
+    else
+        val.to_s + " / " + max.to_s + " " + unit.to_s
+    end
+end
+
+
+
+
 class Value
     def initialize(value=0, min=0, max=0, unit=nil)
         @value= value
@@ -10,8 +28,29 @@ class Value
     end
 
     def value=(new_value)
-        new_value > @max ? @value = new_value : @value = @max
-        new_value < @min ? @value = new_value : @value = @min
+        new_value < @min ? @value = @min : @value = new_value
+        new_value > @max ? @value = @max : @value = new_value
+    end
+
+    def value
+        express(@value, @unit, @max)
+    end
+
+    def max
+        express(@max, @unit)
+    end
+
+    def min
+        express(@min, @unit)
     end
 end
 
+
+
+
+fish = Value.new(1.2354,2,3,"fish")
+puts fish.value
+puts fish.min
+puts fish.max
+fish.value = 1
+puts fish.value
