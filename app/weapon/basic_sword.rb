1d8c8f451d421ff592452e3b6e9bf67a4f13b3c7
ldocao <ldocao@gmail.com>
2015-10-16 18:08:14 +0200

    feat(Battle): initial commit

---
+++ app/weapon/basic_sword.rb
@@ -0,0 +1,9 @@
+module Weapon
+    class BasicSword
+        def initialize(bonus = DEFAULT_STATUS)
+            @name = "Basic Sword"
+            @bonus = bonus
+        end
+
+    end
+end
---
+++ app/weapon/weapon.rb
@@ -0,0 +1,3 @@
+module Weapon
+
+end
