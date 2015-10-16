42524ac63f8706f5f34e2e05adcbc773befe98f4
ldocao <ldocao@gmail.com>
2015-10-16 15:51:21 +0200

    feat: normal attack

--- app/battle/damage.rb
+++ app/battle/damage.rb
@@ -4,10 +4,17 @@ module Battle
     class Damage < Common::Value
         def initialize(value, type=Battle::NORMAL_TYPE) 
             @value = value
-            @min = Stats::MIN_HP
-            @max = Stats::MAX_HP
+            @min = Battle::MIN_DAMAGE
+            @max = Battle::MAX_DAMAGE
             @unit = Battle::DAMAGE_UNIT
             @type = type
+        end
+
+
+        def self.normal(force, target) #eventually add weapon
+            effective_damage = force.value - target.status.defense.value
+            #add here other effect such as armor, weapon
+            Battle::Damage.new(effective_damage)
         end
     end
 end
