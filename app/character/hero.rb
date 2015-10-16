ec562f120014061924106fdefb1c2ad6ccac8d84
ldocao <ldocao@gmail.com>
2015-10-16 12:54:34 +0200

    feat: attack method

--- app/character/hero.rb
+++ app/character/hero.rb
@@ -1,11 +1,13 @@
-=begin
 require_relative "./character.rb"
 require_relative "./human.rb"
 require_relative "../common/value.rb"
 require_relative "../stats/hit_point.rb"
 require_relative "../stats/magic_point.rb"
 require_relative "../stats/level.rb"
-=end
+require_relative "../stats/status.rb"
+require_relative "../stats/stats.rb"
+require_relative "../ennemy/ennemy.rb"
+require_relative "../ennemy/succube.rb"
 
 module Character
 
@@ -16,15 +18,27 @@ module Character
             @first_name = first_name
             @last_name = last_name
             @level = level
-            @stats = Stats::status(level)
+            @status = Stats::Status.new(@level)
         end
 
 
         def to_s
             @first_name+" "+@last_name+" : "+@level.to_s
         end
+
+
+        def attack(target)
+            damage = @status.force.value #-5HP
+            target.status.hit_point -= damage
+        end
     end
 
 
 end
 
+ennemy1 = Ennemy::Succube.new(Stats::Status.new(Stats::Level.new(1))) #objet monstre avec HP, MP, force, defense
+
+zeyn = Character::Hero.new("zeyn", "dupont")
+puts ennemy1.status.hit_point
+zeyn.attack(ennemy1)
+puts ennemy1.status.hit_point #est ce que HP => -5 ?
