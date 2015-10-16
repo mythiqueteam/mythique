4f20c3b8fbc7ad813abc5f5feb7df643ca7c5a42
ldocao <ldocao@gmail.com>
2015-10-16 15:17:51 +0200

    fix: damage is just a value

--- app/battle/damage.rb
+++ app/battle/damage.rb
@@ -2,8 +2,8 @@ require_relative "../common/value.rb"
 
 module Battle
     class Damage < Common::Value
-        def initialize(force, defense, type=Battle::NORMAL_TYPE) 
-            @value = force.value - defense.value
+        def initialize(value, type=Battle::NORMAL_TYPE) 
+            @value = value
             @min = Stats::MIN_HP
             @max = Stats::MAX_HP
             @unit = Battle::DAMAGE_UNIT
